00dbac52988b4be0d909a919133173256de6b030
Otirbepilef <otirbepilef@gmail.com>
2026-08-31 20:14:48 -0300

    other

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.letrinhas}>Open up App.tsx to start working on your app!</Text>
       <Text>Já abri esse caralho de app.tsx </Text>
       <Text>Hello world caralho!1!!11</Text>
       <StatusBar style="auto" />
@@ -16,7 +16,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    color:'#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  letrinhas:{
+    color:'red',
+  }
 });
